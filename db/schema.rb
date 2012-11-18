7a35f67ffac55cbaabe0de72b4f674777fdcc577
brownman <ofer.shaham@gmail.com>
2012-11-18 14:25:12 +0200

    db schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 
 ActiveRecord::Schema.define(:version => 20121029121212) do
 
+
   create_table "comments", :force => true do |t|
     t.text   "content"
     t.datetime "created_at", :null => false
