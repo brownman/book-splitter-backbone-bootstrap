4b359d15038d3e1ac904aaf50ce3c24723a67731
brownman <ofer.shaham@gmail.com>
2012-11-18 14:14:22 +0200

    repair schema: t.text content

--- db/migrate/20120319232556_create_comments.rb
+++ db/migrate/20120319232556_create_comments.rb
@@ -1,8 +1,14 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+
+      t.string :title
+
       t.text :content
 
+      t.boolean :direction
+
+      t.integer :post_id
       t.timestamps
     end
   end
--- db/migrate/20120328010958_remove_content_from_posts.rb
+++
@@ -1,9 +0,0 @@
-class RemoveContentFromPosts < ActiveRecord::Migration
-  def up
-    remove_column :posts, :content
-  end
-
-  def down
-    add_column :posts, :content
-  end
-end
