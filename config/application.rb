e103adb1a6abb440b9be9f0569108566a1c1f6ba
brownman <ofer.shaham@gmail.com>
2012-11-26 13:25:28 +0200

    rails config: rise error

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module RailsBackboneRelational
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.action_mailer.raise_delivery_errors = true
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
