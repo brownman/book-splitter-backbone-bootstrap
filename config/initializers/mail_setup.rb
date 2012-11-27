aa469540beb9983df77ef34484f7ae66a1aef921
brownman <ofer.shaham@gmail.com>
2012-11-27 13:17:38 +0200

    update mail settings

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,13 +1,1 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => "mariana82.herokuapp.com",
-  :address => "smtp.sendgrid.net",
-  :enable_starttls_auto => true
-}
-ActionMailer::Base.delivery_method = :smtp
-
 
